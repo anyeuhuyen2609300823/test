5f8934eb126936ece24dcb3bd3efb459c40bbda5
anyeuhuyen2609300823 <108939314+anyeuhuyen2609300823@users.noreply.github.com>
2022-11-04 22:05:53 +0700

    commit

--- duanmau/admin/home.php
+++ duanmau/admin/home.php
@@ -1,11 +1,3 @@
 <?php
-@session_start();
-if (isset($_SESSION['user'])) {
 
-include ("controllers/c_home.php");
-$home = new c_home();
-$home->index();
-} else {
-    header("location:login.php");
-}
 ?>
